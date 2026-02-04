64889c04afcc1f471bc5ab9fbcd3cd0f231f363d
Antun Matanović <DeVelox@users.noreply.github.com>
2026-02-04 01:10:29 +0100

    Add COSMIC and other apps

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,17 @@
 
 set -ouex pipefail
 
-### Install packages
+# Nightly COSMIC COPR
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+dnf5 -y copr disable ryanabx/cosmic-epoch
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+systemctl disable gdm.service
+systemctl enable cosmic-greeter.service
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Extra apps
+dnf5 -y install fish alacritty quickemu podman-compose
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Cleanup
+dnf5 clean all
+rm -rf /tmp/* || true
