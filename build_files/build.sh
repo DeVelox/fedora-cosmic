ffe8a1593566a8de7b1add452e35d0c0917b6e9b
Antun Matanović <DeVelox@users.noreply.github.com>
2026-03-11 18:50:09 +0100

    Move most CLI packages from brew to Terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,10 @@ systemctl disable gdm.service
 systemctl enable cosmic-greeter.service
 
 # Extra apps
-dnf5 -y install fish alacritty quickemu podman-compose
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 -y install alacritty fish fastfetch podlet podman-compose quickemu starship zellij \
+atuin bat btop dysk eza fd-find helix ncdu ripgrep trash-cli stow tokei ugrep zoxide
+dnf5 -y config-manager setopt terra.enabled=false
 
 # Cleanup
 dnf5 clean all
