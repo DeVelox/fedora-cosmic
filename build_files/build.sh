6b45db8197c8ab024307967cf8072d9856d9641b
Antun Matanović <DeVelox@users.noreply.github.com>
2026-03-12 05:13:04 +0100

    Add direnv

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ systemctl enable cosmic-greeter.service
 
 # Extra apps
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 -y install atuin alacritty bat btop dysk eza fd-find fastfetch fish helix ncdu nix-core \
+dnf5 -y install atuin alacritty bat btop direnv dysk eza fd-find fastfetch fish helix ncdu nix-core \
 podlet podman-compose quickemu ripgrep starship stow tokei trash-cli ugrep zellij zoxide
 
 # Switch to bootc for updates
