693bdad5ee3a9df42994f2377a727892971a35e3
Antun Matanović <DeVelox@users.noreply.github.com>
2026-03-12 08:14:45 +0100

    Disable automatic reboot for bootc updates

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,13 +12,13 @@ systemctl enable cosmic-greeter.service
 
 # Extra apps
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 -y install atuin alacritty bat btop direnv dysk eza fd-find fastfetch fish helix ncdu nix-core \
+dnf5 -y install atuin alacritty bat btop direnv dysk eza fd-find fish helix ncdu nix-core \
 podlet podman-compose quickemu ripgrep starship stow tokei trash-cli ugrep zellij zoxide
 
 # Switch to bootc for updates
-systemctl enable bootc-fetch-apply-updates.timer
 systemctl disable rpm-ostreed-automatic.timer
-sed -i 's/stage/none/' /etc/rpm-ostreed.conf
+systemctl enable bootc-fetch-apply-updates.timer
+sed -i 's/--apply //' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 
 # Cleanup
 dnf5 clean all
